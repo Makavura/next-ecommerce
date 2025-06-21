0646aea0a47fd559a86a85409dec1c1555c8aeed
Makavura <makavuramughanga@gmail.com>
2025-06-21 14:26:50 +0300

    feature: bare bones data fetching

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,13 @@
-export default function Products (){
-    return <div className=""></div>
+import { useQuery } from "@tanstack/react-query";
+
+import { fetchProducts } from "@/app/api/products";
+import { Product } from "@/app/lib/types";
+
+export default function Products() {
+  const {} = useQuery<Product[], Error>({
+    queryKey: ["products"],
+    queryFn: fetchProducts,
+  });
+
+  return <div className=""></div>;
 }
