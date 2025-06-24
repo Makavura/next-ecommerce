0926c1eba2d493789c420dff0bd56cf47f500cb6
Makavura <makavuramughanga@gmail.com>
2025-06-24 16:44:18 +0300

    feature(wip): checkout page

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import * as Yup from "yup";
 import React, { useRef, useEffect, useState } from "react";
+import { Formik, Form } from "formik";
 
 import { anonymousPro } from "@/lib/fonts";
 
@@ -29,6 +31,33 @@ const Checkout = () => {
     console.log("Complete");
   };
 
+  const initialValues = {
+    city: "",
+    phone: "",
+    fullName: "",
+    deliveryInstructions: "",
+    cvv: "",
+    expDate: "",
+    cardName: "",
+    cardNumber: "",
+  };
+
+  const shippingSchema = Yup.object({
+    fullName: Yup.string().required("Full name is required"),
+    city: Yup.string().required("City is required"),
+    phone: Yup.string().required("Phone number is required"),
+    deliveryInstructions: Yup.string(),
+  });
+
+  const paymentSchema = Yup.object({
+    cardNumber: Yup.string().required("Card number is required"),
+    expDate: Yup.string().required("Expiration date is required"),
+    cvv: Yup.string().required("CVV is required"),
+    cardName: Yup.string().required("Card holder name is required"),
+  });
+
+  const combinedSchema = shippingSchema.concat(paymentSchema);
+
   return (
     <div className="container mx-auto pt-5">
       <div className="flex gap-2 mb-4 w-full justify-between">
@@ -44,7 +73,34 @@ const Checkout = () => {
         ))}
       </div>
 
-      <div className="flex gap-2 w-full justify-between bg-white shadow-2xl p-5 border-[0.5px] border-slate-500">
+      <div className="bg-white shadow-2xl p-5 border-[0.5px] border-slate-500">
+        <Formik
+          initialValues={initialValues}
+          validationSchema={combinedSchema}
+          onSubmit={(values) => {
+            console.log("Submitted data:", values);
+          }}
+        >
+
+          {({ 
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            values, setFieldValue }) => (
+            <Form className="space-y-6 w-full">
+              {currentStep === 0 && (
+                <>
+
+                </>
+              )}
+              {currentStep === 1 && (
+                <>
+
+                </>
+              )}
+              {currentStep === 2 && <div className="">Purchase summary</div>}
+            </Form>
+          )}
+        </Formik>
+        <div className="flex gap-2 w-full justify-between">
         <button
           className={`${anonymousPro.className} mt-3 bg-gray-800 text-white px-5 py-2 rounded-none shadow-md hover:bg-gray-700 transition duration-300 transform hover:-translate-y-0.5`}
           onClick={handlePrevious}
@@ -67,6 +123,7 @@ const Checkout = () => {
             Complete purchase
           </button>
         )}
+        </div>
       </div>
     </div>
   );
