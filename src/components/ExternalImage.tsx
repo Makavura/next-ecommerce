7667addbeb461255d3c7f7240030b073154a9fbd
Makavura <makavuramughanga@gmail.com>
2025-06-24 16:52:57 +0300

    refactor/update: include alt as props for type checking

--- src/components/ExternalImage.tsx
+++ src/components/ExternalImage.tsx
@@ -5,8 +5,8 @@ const externalLoader = ({ src }: ImageLoaderProps) => {
   return src;
 };
 
-export default function ExternalImage(
-  props: Omit<React.ComponentProps<typeof Image>, "loader">
-) {
-  return <Image loader={externalLoader} {...props} />;
+type ExternalImageProps = Omit<React.ComponentProps<typeof Image>, "loader">;
+
+export default function ExternalImage({ alt, ...props }: ExternalImageProps) {
+  return <Image loader={externalLoader} alt={alt} {...props} />;
 }
