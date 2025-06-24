55b620c68e84cd18206c226262285448f35caddf
Makavura <makavuramughanga@gmail.com>
2025-06-24 19:09:10 +0300

    fix: svg path attributes

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -112,8 +112,8 @@ export default function Products() {
               stroke-width="2"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
               />
             </svg>
