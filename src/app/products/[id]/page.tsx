8ba7170609c87fcbd040587e54e5bc6eb403d940
Makavura <makavuramughanga@gmail.com>
2025-06-25 13:09:43 +0300

    chore: include types jest and node so that TypeScript will be aware of global types like jest.Mock

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,4 +1,7 @@
+import fs from "fs";
+import path from "path";
 import axios from "axios";
+
 import { IProduct } from "@/lib/types";
 import ProductItemCard from "@/components/ProductItemCard";
 
@@ -6,8 +9,28 @@ type TProductItemParams = {
   id: string;
 };
 
+const MOCK_FILE_NAME = "mockProducts.json";
+const MOCK_FILE_PATH = path.join(
+  "./",
+  "src",
+  "utils/tests",
+  MOCK_FILE_NAME
+);
+
+const writeMockProducts = (products: IProduct[]) => {
+  const dir = path.dirname(MOCK_FILE_PATH);
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+
+  fs.writeFileSync(MOCK_FILE_PATH, JSON.stringify(products, null, 2), "utf-8");
+  console.table(products);
+  console.log(
+    `Successfully saved ${products.length} products to ${MOCK_FILE_PATH}`
+  );
+};
+
 export async function generateStaticParams() {
-  
   const response = await axios.get<IProduct[]>(
     `https://api.escuelajs.co/api/v1/products`
   );
@@ -18,14 +41,19 @@ export async function generateStaticParams() {
 
   const products = response.data;
 
+  writeMockProducts(products);
+
   return products?.map((product) => ({
     id: product.id.toLocaleString(),
   }));
 }
 
-
-const ProductItem = async ({ params }: {params: Promise<TProductItemParams>}) => {
-  const productId = (await params).id
+const ProductItem = async ({
+  params,
+}: {
+  params: Promise<TProductItemParams>;
+}) => {
+  const productId = (await params).id;
   return <ProductItemCard productId={productId} />;
 };
 
